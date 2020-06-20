fc0bb0ef70115e4d84deb42935414b96656548f9
alonso pacheco <capacheco@ucsp.edu.pe>
2020-06-20 08:12:48 -0500

    Add project API

---
+++ IntegracionAPI/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntegracionAPI.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        #region Select
+        [HttpPost("auticacion")]
+        public IActionResult api_autenticacion([FromHeader]string Usuario, [FromHeader]string Contrasenia, ObjAutenticacion request)
+        {
+            if (Usuario == "Jean")
+            {
+                return Ok("Ok");
+            }
+            if (request.usuario == "Jean")
+            {
+                return Ok("Ok");
+            }
+            return Ok("Ok");
+
+          //  fetch('https://jsonplaceholder.typicode.com/todos', {
+          //  method: 'POST',
+          //  body: JSON.stringify({
+          //      usuario: "Jean",             
+          //      constrasenia: "xxxx"
+          //  }),
+          //  headers:
+          //      {
+          //          "Content-type": "application/json",
+          //          "Usuario": "jean",
+          //          "Constrasenia": "xxxx"
+          //  })
+          //.then(response => response.json())
+          //.then(json => console.log(json))
+
+        }
+        #endregion
+
+        #region Insert
+
+        #endregion
+
+        #region Update
+
+        #endregion
+
+        #region Delete
+
+        #endregion
+    }
+}
---
+++ IntegracionAPI/Models/ObjRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegracionAPI.Models
+{
+    public class ObjRequest
+    {
+        
+    }
+
+    public class ObjAutenticacion
+    {
+        public string usuario { get; set; }
+        public string constrasenia { get; set; }
+    }
+}
